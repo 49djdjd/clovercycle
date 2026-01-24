73f1e5fd6838d044fce846279a70f7db5f334f31
Azra Ali <azra_ali09@outlook.com>
2026-01-24 18:59:56 -0500

    made the navbar responsive (to different screen sizes)

--- clovercycle/app/page.tsx
+++ clovercycle/app/page.tsx
@@ -4,12 +4,14 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col font-sans bg-[#fffdf1]">
       <main className="flex mx-auto justify-between items-center w-full py-0 bg-[#fffdf1]">
-        <ul className = "bg-[#000000] list-none flex justify-between m-0 p-2 gap-4 w-full">
-        <li><a href=""className="block text-2xl text-[#258730] p-[10px] ml-1 no-underline hover:bg-[#F7F7D2]">Home</a></li>
-        <li><a href=""className="block text-2xl text-[#258730] p-[10px] no-underline hover:bg-[#F7F7D2]">Survey</a></li>
-        <li><a href=""className="block text-2xl text-[#258730] p-[10px] no-underline hover:bg-[#F7F7D2]">Research</a></li>
-        <li><a href=""className="block text-2xl text-[#258730] p-[10px] no-underline hover:bg-[#F7F7D2]">References</a></li>
-        <li><a href=""className="block text-2xl text-[#258730] p-[10px] mr-1 no-underline hover:bg-[#F7F7D2]">Contact Us</a></li>
+        <ul className = "bg-cover bg-top sticky flex list-none flex-wrap justify-between m-0 p-4 gap-4 w-full"
+        //refernced ai for this line
+        style={{ backgroundImage: "url('/images/cloverbg.png')" }}>
+        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Home</a></li>
+        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Survey</a></li>
+        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Research</a></li>
+        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">References</a></li>
+        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Contact Us</a></li>
         </ul>
       </main>
     </div>
