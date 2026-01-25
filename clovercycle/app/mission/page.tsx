40fb8ece045b42cad4b3e5fb02d4c103a82a513f
Azra Ali <azra_ali09@outlook.com>
2026-01-25 12:06:35 -0500

    working on getting the right size for the arch text/image

---
+++ clovercycle/app/mission/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+return (
+    <div className="flex min-h-screen flex-col font-sans bg-[#fffdf1]">
+    <main className="flex flex-col mx-auto justify-between items-center w-full py-0 bg-[#fffdf1]">
+        <ul className = "bg-cover bg-top sticky flex list-none flex-wrap justify-between m-0 p-4 gap-4 w-full"
+        style={{ backgroundImage: "url('/images/cloverbg.png')" }}>
+        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2"><Image src="/images/cloverlogo.png" alt="clover" width={50} height ={50} className="object-contain"/></a></li>
+        <li><Link href="/"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Home</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Mission</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Research</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Initatives</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Contact Us</Link></li>
+        </ul>
+    <div>
+        <p className="text-base md:text-lg text-black">
+            Clovercycle is a student led organization meant to spread awareness about the importance of enviornmental sustanability and it's impact on human health
+        </p>
+    </div>
+    </main>
+    </div>
+
+);
+}
--- clovercycle/app/page.tsx
+++ clovercycle/app/page.tsx
@@ -1,25 +1,58 @@
 import Image from "next/image";
 import Script from "next/script";
+import Link from "next/link";
 
 export default function Home() {
   return (
+    //refernced ai for this line
+    <>
+    <Script src="https://www.instagram.com/embed.js" strategy="afterInteractive"/>
     <div className="flex min-h-screen flex-col font-sans bg-[#fffdf1]">
       <main className="flex flex-col mx-auto justify-between items-center w-full py-0 bg-[#fffdf1]">
         <ul className = "bg-cover bg-top sticky flex list-none flex-wrap justify-between m-0 p-4 gap-4 w-full"
         //refernced ai for this line
         style={{ backgroundImage: "url('/images/cloverbg.png')" }}>
-        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2"><img src="/images/cloverlogo.png" className="w-[50px] h-[50px] object-contain"/></a></li>
-        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Home</a></li>
-        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Survey</a></li>
-        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Research</a></li>
-        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">References</a></li>
-        <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Contact Us</a></li>
+                <li><a href=""className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2"><Image src="/images/cloverlogo.png" alt="clover" width={50} height ={50} className="object-contain"/></a></li>
+        <li><Link href="/"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Home</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Mission</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Research</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Initatives</Link></li>
+        <li><Link href="/mission"className="block text-base md:text-2xl text-[#258730] p-2 md:p-[10px] ml-2 hover:scale-105">Contact Us</Link></li>
         </ul>
         {/* //reference ai for this div (needed help making it appear with tailwind) */}
-        <div className="flex justify-center items-center p-8">
-          <blockquote className="instagram-media mx-auto my-px min-w-[326px] max-w-[540px] w-[calc(100%-2px)] rounded border-0 bg-white p-0 shadow-[0_0_1px_0_rgba(0,0,0,0.5) 0_1px_10px_0_rgba(0,0,0,0.15)]" data-instgrm-permalink="https://www.instagram.com/embroidery_for_everyone/?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="14"></blockquote>
+        {/* <div className="flex justify-center items-center p-12">
+          <blockquote className="instagram-media transform scale-110 md:scale-150 mx-auto my-px min-w-[506px] max-w-[800px] w-full rounded border-0 bg-white p-0 shadow-[0_0_1px_0_rgba(0,0,0,0.5),0_1px_10px_0_rgba(0,0,0,0.15)]" data-instgrm-permalink="https://www.instagram.com/embroidery_for_everyone/?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="14"></blockquote>
+          <a href="https://ww.instgram.com/embroidery_for_everyone/"></a>
+        </div> */}
+        <div className = "mt-6 flex flex-col items-center">
+          {/*referenced ai for help with the svg*/}
+        <svg
+        viewBox="0 0 600 360"
+        className="w-full h-auto"
+      >
+        <path
+          id="arc"
+          d="M20,280 A280,280 0 0,1 580,300"
+          fill="none"
+        />
+        <text
+          fontSize="120"
+          fill="#258730"
+          fontWeight="700"
+        >
+          <textPath
+            href="#arc"
+            startOffset="50%"
+            textAnchor="middle"
+          >
+            Live Green. Live Well.
+          </textPath>
+        </text>
+      </svg>
+      <Image src="/images/clovercyclelogo.png" alt="logo with name" width={500} height={500} className="object-contain flex justify-center"></Image>
         </div>
       </main>
     </div>
+    </>
   );
 }
