e07745a289120f8ad442aeb45ceb17dcd4d818f6
Azra Ali <azra_ali09@outlook.com>
2026-01-25 12:22:58 -0500

    got the cloverimage to be under the arch, working on centering it

--- clovercycle/app/page.tsx
+++ clovercycle/app/page.tsx
@@ -24,19 +24,19 @@ export default function Home() {
           <blockquote className="instagram-media transform scale-110 md:scale-150 mx-auto my-px min-w-[506px] max-w-[800px] w-full rounded border-0 bg-white p-0 shadow-[0_0_1px_0_rgba(0,0,0,0.5),0_1px_10px_0_rgba(0,0,0,0.15)]" data-instgrm-permalink="https://www.instagram.com/embroidery_for_everyone/?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="14"></blockquote>
           <a href="https://ww.instgram.com/embroidery_for_everyone/"></a>
         </div> */}
-        <div className = "mt-6 flex flex-col items-center">
+        <div className ="flex flex-col items-center relative">
           {/*referenced ai for help with the svg*/}
         <svg
-        viewBox="0 0 600 360"
-        className="w-full h-auto"
+        viewBox="0 0 300 130"
+        className="w-300 h-auto"
       >
         <path
           id="arc"
-          d="M20,280 A280,280 0 0,1 580,300"
+          d="M10,180 A140,140 0 0,1 290,180"
           fill="none"
         />
         <text
-          fontSize="120"
+          fontSize="32"
           fill="#258730"
           fontWeight="700"
         >
@@ -49,7 +49,7 @@ export default function Home() {
           </textPath>
         </text>
       </svg>
-      <Image src="/images/clovercyclelogo.png" alt="logo with name" width={500} height={500} className="object-contain flex justify-center"></Image>
+      <Image src="/images/clovercyclelogo.png" alt="logo with name" width={500} height={500} className="object-contain flex -mt-[280px]"></Image>
         </div>
       </main>
     </div>
